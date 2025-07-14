8c1d75b489c9ca43a3eb17a798d439111a52de14
Huan Nguyen <famish99@gmail.com>
2025-07-13 22:18:37 -0500

    add amplify config to code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Amplify } from "aws-amplify";
+import outputs from "../../amplify_outputs.json";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -11,6 +13,8 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
+Amplify.configure(outputs);
 
 export const metadata: Metadata = {
   title: "Create Next App",
